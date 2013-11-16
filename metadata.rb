c5e6f21b67d828e35df328aa1bad17ca24df11e1
Philip Hutchins <flipture@gmail.com>
2013-11-16 18:25:50 -0500

    added osx support

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ description		"Installs a simple http listener to start a chef run"
 maintainer		"Philip Hutchins"
 maintainer_email 	"philip.hutchins@dealermatch.com"
 long_description 	IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          	"0.0.1"
+version          	"0.0.2"
